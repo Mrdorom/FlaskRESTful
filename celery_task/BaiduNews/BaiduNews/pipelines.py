a13d7ed1dee8bf9a57305d76c8be483e0a64f9b6
WRAllen <1072274105@qq.com>
2020-04-03 17:27:06 +0800

    修改爬虫测试

--- celery_task/BaiduNews/BaiduNews/pipelines.py
+++ celery_task/BaiduNews/BaiduNews/pipelines.py
@@ -1,11 +1,30 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
 
 
 class BaidunewsPipeline(object):
+    def __init__(self):
+        self.db = pymysql.connect("localhost", "root",
+                                  "AliCentOSMysql123456", "FlaskRESTFul")
+        self.cursor = self.db.cursor()
+
+    def open_spider(self, spider):
+        print("开启爬虫！！！")
+
     def process_item(self, item, spider):
+        print("=============================")
+        print(f"结果：{item}")
+        # 使用execute()方法执行SQL语句
+        self.cursor.execute("SELECT * FROM user")
+        # 使用fetall()获取全部数据
+        data = self.cursor.fetchall()
+        # 打印获取到的数据
+        print("-----------", data)
         return item
+
+    def close_spider(self, spider):
+        # 关闭游标和数据库的连接
+        self.cursor.close()
+        # 如果有修改增加数据等需要commit
+        # db.commit()
+        self.db.close()
+        print("结束")
